99bbfb139c437c13459eff8017df4bc20f8918fd
Jedidiah Hurt <jed.hurt@gmail.com>
2015-06-19 22:29:37 -0600

    Implement product options modals

--- _assets/javascripts/products.js
+++ _assets/javascripts/products.js
@@ -1,4 +1,6 @@
 (function () {
+  'use strict';
+
   var initProductionVariationLinks = function () {
     $('.product-variation a').click(function (event) {
       var link = $(event.target);
@@ -9,7 +11,69 @@
     });
   };
 
+  var highlightProductOption = function (siblingElements, element) {
+    if (!siblingElements) return;
+    siblingElements.removeClass('chosen');
+    $(event.target).addClass('chosen');
+  }
+
+  var selectedOption = function (selectors) {
+    return $(_.find(selectors, function (selector) { return $(selector).hasClass('chosen') }))
+      .attr('data-label');
+  };
+
+  var selectedSelectBoxOption = function (selectBox) {
+    return selectBox.val();
+  };
+
+  var optionName = function (filmType, shirtSize, shirtDesign) {
+    return filmType + ' & ' + shirtSize + ' ' + shirtDesign + ' shirt';
+  };
+
+  var lookupOptionId = function (productData, productId, optionName) {
+    var product = _.findWhere(productData, { id: productId });
+    return _.findWhere(product.options, { name: optionName }).id;
+  };
+
+  var initProductOptionsModal = function (productData, _, _modal) {
+    var modal = $(_modal);
+
+    var productId = parseInt(modal.attr('data-product-id'), 10);
+    var form = modal.closest('form');
+    // TODO: Rehydrate the UI based on the retained form values when a user
+    // navigates 'back' to the home page. In the meantime, punt by resetting
+    // the form.
+    form.get(0).reset();
+
+    var productIdInput = form.find('input.product-id');
+
+    var optionChanged = function (optionElements, event) {
+      highlightProductOption(optionElements, $(event.target));
+      var optionId = lookupOptionId(productData, productId, optionName(selectedOption(filmTypeSelectors),
+                                                                       selectedSelectBoxOption(shirtSizeSelector),
+                                                                       selectedOption(shirtDesignSelectors)));
+      productIdInput.val(optionId);
+    };
+
+    var filmTypeSelectors = modal.find('.film-type img');
+    filmTypeSelectors.click(optionChanged.bind(null, filmTypeSelectors));
+
+    var shirtDesignSelectors = modal.find('.shirt-design img');
+    shirtDesignSelectors.click(optionChanged.bind(null, shirtDesignSelectors));
+
+    var shirtSizeSelector = modal.find('.shirt-size select');
+                                                      // pass 'null' for optionElements arg as
+                                                      // the select box doesn't have sibling elements
+    shirtSizeSelector.change(optionChanged.bind(null, null));
+  };
+
+  var initProductOptionsModals = function (productData) {
+    $('.product-options-modal').each(initProductOptionsModal.bind(null, productData));
+  };
+
   $(function () {
     initProductionVariationLinks();
+    $.get('/_data/bc_products.json')
+      .done(initProductOptionsModals);
   });
 }());
