65e91e85c239919e287da92c8032e57a0ca1ac8a
Jedidiah Hurt <jed.hurt@gmail.com>
2015-06-19 23:18:23 -0600

    Fix bug in t-shirt options selectors

--- _assets/javascripts/products.js
+++ _assets/javascripts/products.js
@@ -5,7 +5,7 @@
     $('.product-variation a').click(function (event) {
       var link = $(event.target);
       var formTag = link.closest('form');
-      var inputTag = formTag.find('input#option');
+      var inputTag = formTag.find('input.product-id');
       inputTag.val(link.attr('data-product-id'));
       formTag.submit();
     });
